77876b7207d3a45f9eea02fa569c0652a8b40eaa
DevHimanshu63 <hsc95067@gmail.com>
2022-03-25 16:07:40 +0530

    all matches venue date recieved

--- scoreCard.js
+++ scoreCard.js
@@ -2,14 +2,30 @@ const request =require("request");
 const cheerio=require("cheerio");
 
 
+
 function getInfofromMatch(url){
-    console.log("from all scorecarc.js"+"   "+  url);
-    // request(url,cb);
+    // console.log("from all scorecarc.js"+"   "+  url);
+    request(url,cb);
 
 }
 
+function cb(err , res ,html){
+    if(err){
+        console.log(err);
+    }else{
+        matchdetail(html);
+    }
+}
+
+function matchdetail(html){
+    let selecTool=cheerio.load(html)
+    // load html of all match
+    // console.log(selecTool.text());
+    let descArray=selecTool(".match-header-info.match-info-MATCH")
+    console.log(descArray.text());
+}
 
 module.exports={
     getInfofromMatch:getInfofromMatch,
-    
+
 }
